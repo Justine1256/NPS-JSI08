42215f0be38e2e5e4510db25cc56d52c362a6d31
unknown <huynhphamthuyanh.126@gmail.com>
2022-11-22 23:07:10 +0700

    done js, layout

--- LESSON 4/Bài kiểm tra/index.js
+++ LESSON 4/Bài kiểm tra/index.js
@@ -7,6 +7,13 @@ const list = [
       "https://salt.tikicdn.com/cache/280x280/ts/product/90/55/ea/340eb77f1170e4c381c866c275138a82.jpg.webp",
   },
   {
+    name: "Tai nghe JBL C150SI",
+    price: 235000,
+    type: "earphone",
+    image:
+      "https://salt.tikicdn.com/cache/280x280/ts/product/37/f0/49/fc18ee8fbf3a5f8b76b1b374c0db8851.jpg.webp",
+  },
+  {
     name: "Chuột không dây Logitech M220 Silent",
     price: 219000,
     type: "mouse",
@@ -14,18 +21,11 @@ const list = [
       "https://salt.tikicdn.com/cache/280x280/ts/product/33/82/18/5b3a94d00db81dcc6a7a2a8bd62cad04.jpg.webp",
   },
   {
-    name: "Giá đỡ hợp kim nhôm Tamayoko",
-    price: 369000,
-    type: "accessor",
+    name: "Chuột chơi game G5",
+    price: 509000,
+    type: "mouse",
     image:
-      "https://salt.tikicdn.com/cache/280x280/ts/product/fd/04/cc/bba5a17a5be8d77aa8ba0346c4ced3d9.png.webp",
-  },
-  {
-    name: "Sạc dự phòng không dây Mophie",
-    price: 1150000,
-    type: "portable charger",
-    image:
-      "https://salt.tikicdn.com/cache/280x280/ts/product/e3/dd/f2/4c8b4fcacb067c084dcfa14d58d57b4e.jpg.webp",
+      "https://salt.tikicdn.com/cache/280x280/ts/product/aa/e7/92/f6767d13b63b70c89e2e39360cff84c0.jpg.webp",
   },
   {
     name: "Bàn phím gấp 3 ZAGG Universal Keyboards",
@@ -35,6 +35,13 @@ const list = [
       "https://salt.tikicdn.com/cache/280x280/ts/product/a1/40/36/80c26fdf3f02e2f61f711030ace3bc74.png.webp",
   },
   {
+    name: "Samsung Galaxy S20 FE",
+    price: 8790000,
+    type: "smartphone",
+    image:
+      "https://salt.tikicdn.com/cache/280x280/ts/product/98/28/4a/7ec6ad5943179c4b2ad5ae8b79798599.jpeg.webp",
+  },
+  {
     name: "Apple iphone 14 pro max",
     price: 33490000,
     type: "smartphone",
@@ -42,16 +49,9 @@ const list = [
       "https://salt.tikicdn.com/cache/280x280/ts/product/0c/dd/e7/e6431984901119a1f8166cc4e579da93.png.webp",
   },
   {
-    name: "Samsung Galaxy S20 FE",
-    price: 8790000,
-    type: "keyboards",
-    image:
-      "https://salt.tikicdn.com/cache/280x280/ts/product/98/28/4a/7ec6ad5943179c4b2ad5ae8b79798599.jpeg.webp",
-  },
-  {
     name: "Điện thoại Oppo A55",
     price: 3750000,
-    type: "keyboards",
+    type: "smartphone",
     image:
       "https://salt.tikicdn.com/cache/280x280/ts/product/6f/b1/16/2a0e5c3f718d20d998cd13ccc84864b0.jpg.webp",
   },
@@ -80,7 +80,7 @@ function render(productsList) {
           <li class="product">
             <div class="product-img"><img src="${element.image}"></div>
             <span class="product-name">${element.name}</span>
-            <span class="product-price">${element.price}</span>
+            <span class="product-price">${element.price}đ</span>
           </li>
       `
     );
@@ -88,78 +88,98 @@ function render(productsList) {
 }
 render(products);
 
-// const ascBtn = document.getElementById("asc");
-// const descBtn = document.getElementById("desc");
+const ascBtn = document.getElementById("asc");
+const descBtn = document.getElementById("desc");
 
-// // onclick là thuộc tính của thẻ button, chứ ko phải hàm
-// // Onclick được gán bằng 1 function, fucntion này sẽ chỉ có 1 tham số duy nahát đại diện cho sự kiện xảy ra
-// ascBtn.onclick = function () {
-//   // Sắp xếp thứ tự giá tăng dần
-//   const sortedProducts = products.sort(function (a, b) {
-//     // Nếu giá tiền bằng nhau thì không cần đổi chỗ a và b => return 0
-//     if (a.price == b.price) {
-//       return 0;
-//     } else if (a.price > b.price) {
-//       // Nếu giá tiền của a lớn hơn b, thì đổi chỗ a và b (trả về một số lớn hơn 0) => return 1
-//       return 1;
-//     } else {
-//       // Nếu giá tiền của a nhỏ hơn b, thì đổi chỗ a và b => return -1 (sắp xếp a đứng trước b)
-//       return -1;
-//     }
-//   });
+// onclick là thuộc tính của thẻ button, chứ ko phải hàm
+// Onclick được gán bằng 1 function, fucntion này sẽ chỉ có 1 tham số duy nahát đại diện cho sự kiện xảy ra
+ascBtn.onclick = function () {
+  // Sắp xếp thứ tự giá tăng dần
+  const sortedProducts = products.sort(function (a, b) {
+    // Nếu giá tiền bằng nhau thì không cần đổi chỗ a và b => return 0
+    if (a.price == b.price) {
+      return 0;
+    } else if (a.price > b.price) {
+      // Nếu giá tiền của a lớn hơn b, thì đổi chỗ a và b (trả về một số lớn hơn 0) => return 1
+      return 1;
+    } else {
+      // Nếu giá tiền của a nhỏ hơn b, thì đổi chỗ a và b => return -1 (sắp xếp a đứng trước b)
+      return -1;
+    }
+  });
 
-//   //   Sau khi sắp xếp xong, render lại danh sách sản phẩm
-//   render(sortedProducts);
-// };
+  //   Sau khi sắp xếp xong, render lại danh sách sản phẩm
+  render(sortedProducts);
+};
 
-// descBtn.onclick = function () {
-//   // Sắp xếp thứ tự giá tăng dần
-//   const sortedProducts = products.sort(function (a, b) {
-//     // Nếu giá tiền bằng nhau thì không cần đổi chỗ a và b => return 0
-//     if (a.price == b.price) {
-//       return 0;
-//     } else if (a.price > b.price) {
-//       // Nếu giá tiền của a lớn hơn b, thì đổi chỗ a và b (trả về một số nhỏ hơn 0) => return -1
-//       return -1;
-//     } else {
-//       // Nếu giá tiền của a nhỏ hơn b, thì đổi chỗ a và b => return 1 (sắp xếp a đứng sau b)
-//       return 1;
-//     }
-//   });
+descBtn.onclick = function () {
+  // Sắp xếp thứ tự giá tăng dần
+  const sortedProducts = products.sort(function (a, b) {
+    // Nếu giá tiền bằng nhau thì không cần đổi chỗ a và b => return 0
+    if (a.price == b.price) {
+      return 0;
+    } else if (a.price > b.price) {
+      // Nếu giá tiền của a lớn hơn b, thì đổi chỗ a và b (trả về một số nhỏ hơn 0) => return -1
+      return -1;
+    } else {
+      // Nếu giá tiền của a nhỏ hơn b, thì đổi chỗ a và b => return 1 (sắp xếp a đứng sau b)
+      return 1;
+    }
+  });
 
-//   //   Sau khi sắp xếp xong, render lại danh sách sản phẩm
-//   render(sortedProducts);
-// };
+  //   Sau khi sắp xếp xong, render lại danh sách sản phẩm
+  render(sortedProducts);
+};
 
-// // Lọc sản phẩm theo thể loại
-// const filterSelect = document.getElementById("filter");
-// filterSelect.onchange = function () {
-//   const typeSelected = filterSelect.value;
-//   let filteredProducts = [];
-//   switch (typeSelected) {
-//     case "sport":
-//       filteredProducts = products.filter(function (element) {
-//         if (element.type == "sport") {
-//           return true;
-//         } else {
-//           return false;
-//         }
-//       });
-//       break;
+// Lọc sản phẩm theo thể loại
+const filterSelect = document.getElementById("filter");
+filterSelect.onchange = function () {
+  const typeSelected = filterSelect.value;
+  let filteredProducts = [];
+  switch (typeSelected) {
+    case "earphone":
+      filteredProducts = products.filter(function (element) {
+        if (element.type == "earphone") {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      break;
 
-//     case "casual":
-//       filteredProducts = products.filter(function (element) {
-//         if (element.type == "casual") {
-//           return true;
-//         } else {
-//           return false;
-//         }
-//       });
-//       break;
+    case "mouse":
+      filteredProducts = products.filter(function (element) {
+        if (element.type == "mouse") {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      break;
 
-//     default:
-//       filteredProducts = products;
-//       break;
-//   }
-//   render(filteredProducts);
-// };
+      case "keyboards":
+      filteredProducts = products.filter(function (element) {
+        if (element.type == "keyboards") {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      break;
+
+      case "smartphone":
+        filteredProducts = products.filter(function (element) {
+          if (element.type == "smartphone") {
+            return true;
+          } else {
+            return false;
+          }
+        });
+        break;
+
+    default:
+      filteredProducts = products;
+      break;
+  }
+  render(filteredProducts);
+};
